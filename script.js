28c11aff59a06f1e41bebaca10948b875cebd7f8
Ronyedson de Muoura Santos <ronyedson.moura@gmail.com>
2020-12-03 20:11:11 -0300

    Added a start menu and a option to play without the wall's colision Added a max score counter but it is kinda useless since the game doesn't have a way to restart after the game over yet

--- script.js
+++ script.js
@@ -20,14 +20,25 @@ let fly = { x : 0, y : 0};
 let dx = 10;
 let dy = 0;
 
+var wallColision = true;
+
 let score = 0;
+var maxScore = 0;
 
 const snakeboard = document.getElementById('gameCanvas');
 const snakeboard_ctx = snakeboard.getContext('2d');
 
+snakeboard_ctx.strokeStyle = board_border;
+snakeboard_ctx.strokeRect(0, 0, snakeboard.width, snakeboard.height);
+
 //Defines where the fly will be and the starts the game
-createFly();
-main();
+function start() {
+    hideMainMenu();
+    createFly();
+    drawnFly();
+    main();
+};
+
 
 //Event listener to catch the player's inputs
 document.addEventListener('keydown', changeDirection);
@@ -43,8 +54,9 @@ function main() {
     setTimeout(function onTick() {
         
         clearBoard();
-        moveSnake();
         checkPoint();
+        moveSnake();
+        
         drawnSnake();
         drawnFly();
 
@@ -54,6 +66,19 @@ function main() {
 
 }
 
+function hideMainMenu() {
+    document.getElementsByClassName("startMenu")[0].style.zIndex = "0";
+}
+
+function toggleWallColision() {
+    if (wallColision) {
+        wallColision = false;
+        document.getElementById("colision").innerHTML = "WALL COLISION: OFF"
+    } else {
+        wallColision = true;
+        document.getElementById("colision").innerHTML = "WALL COLISION: ON"
+    }
+}
 
 function clearBoard() {
     snakeboard_ctx.fillStyle = board_background;
@@ -83,7 +108,19 @@ function drawnSnakePart(snakePart) {
 }
 
 function moveSnake() {
+    if (snake[0].x > 390) {
+        snake[0].x = 0;
+    }
+    if (snake[0].x < 0) {
+        snake[0].x = 390;
+
+    }if (snake[0].y > 390) {
+        snake[0].y = 0;
+    }if (snake[0].y < 0) {
+        snake[0].y = 390;
+    }
     const head = { x: snake[0].x + dx, y: snake[0].y + dy };
+    
     snake.unshift(head);
     snake.pop();
 }
@@ -127,14 +164,23 @@ function changeDirection(event) {
 
 function has_game_ended() {
     //Checks if the snake has collided with it's own body
-    for (let i = 3; i < snake.length; i++) {
-        if (snake[0].x === snake[i].x && snake[0].y === snake[i].y){
-            return true;
+    if(wallColision){
+        for (let i = 3; i < snake.length; i++) {
+            if (snake[0].x === snake[i].x && snake[0].y === snake[i].y){
+                return true;
+            }
+        }
+        
+        //Checks if the snake has collided with the walls
+        return snake[0].x < 0 || snake[0].y < 0 || snake[0].x == snakeboard.width || snake[0].y == snakeboard.height 
+    } else {
+        for (let i = 3; i < snake.length; i++) {
+            if (snake[0].x === snake[i].x && snake[0].y === snake[i].y){
+                return true;
+            }
         }
     }
     
-    //Checks if the snake has collided with the walls
-    return snake[0].x < 0 || snake[0].y < 0 || snake[0].x == snakeboard.width || snake[0].y == snakeboard.height 
 }
 
 function checkPoint() {
@@ -143,6 +189,10 @@ function checkPoint() {
         snake.unshift({x:fly.x, y:fly.y});
         createFly();
         score++;
+        if (score > maxScore) { 
+            maxScore = score;
+            document.getElementById("maxScore").innerHTML = `MAX SCORE: ${maxScore}`;
+        }
 
         document.getElementById("score").innerHTML = `SCORE: ${score}`;
     }
@@ -158,7 +208,6 @@ function createFly() {
     //Checks if the fly has been generated inside the snake's body and if it is, calls the function again
     for (let i = 0; i < snake.length ; i++) {
         if (fly.x === snake[i].x && fly.y === snake[i].y){
-            console.log('a')
             createFly();
         }
     }
